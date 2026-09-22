ac3e63772dc2f11ac388848ff7ecb3d488b0531b
Abhijeet Kumar <68841353+TEPg-Git@users.noreply.github.com>
2026-09-22 11:57:06 +0530

    Simplify Pong player input

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,54 +1,29 @@
 use winit::keyboard::KeyCode;
 
+// ============================================================
+// KEYBOARD STATE
+// ============================================================
+
 #[derive(Default)]
 pub struct KeyboardState {
-    //Represents the state of the keyboard.
     // ========================================================
-    // OBJECT MOVEMENT
+    // PLAYER 1
     // ========================================================
-    pub w: bool, // Moves the object forward.
-    pub s: bool, // Moves the object backward.
-    pub a: bool, // Moves the object left.
-    pub d: bool, // Moves the object right.
+    pub w: bool,
+    pub s: bool,
 
     // ========================================================
-    // CAMERA MOVEMENT
+    // PLAYER 2
     // ========================================================
-    pub up: bool,    // Moves the camera up.
-    pub down: bool,  // Moves the camera down.
-    pub left: bool,  // Moves the camera left.
-    pub right: bool, // Moves the camera right.
-
-    // ========================================================
-    // CAMERA ZOOM
-    // ========================================================
-    pub zoom_in: bool,  // Zooms in the camera.
-    pub zoom_out: bool, // Zooms out the camera.
-
-    // ========================================================
-    // SPEED
-    // ========================================================
-    pub i: bool, // Increases the speed of the object.
-    pub o: bool, // Decreases the speed of the object.
-
-    // ========================================================
-    // ROTATION
-    // ========================================================
-    pub q: bool, // Rotates the object counter-clockwise.
-    pub e: bool, // Rotates the object clockwise.
-
-    // ========================================================
-    // SCALE
-    // ========================================================
-    pub z: bool, // Increases the scale of the object.
-    pub x: bool, // Decreases the scale of the object.
+    pub up: bool,
+    pub down: bool,
 }
 
 impl KeyboardState {
     pub fn handle_keyboard(&mut self, key_code: KeyCode, pressed: bool) {
         match key_code {
             // ------------------------------------------------
-            // OBJECT MOVEMENT
+            // PLAYER 1
             // ------------------------------------------------
             KeyCode::KeyW => {
                 self.w = pressed;
@@ -58,16 +33,8 @@ impl KeyboardState {
                 self.s = pressed;
             }
 
-            KeyCode::KeyA => {
-                self.a = pressed;
-            }
-
-            KeyCode::KeyD => {
-                self.d = pressed;
-            }
-
             // ------------------------------------------------
-            // CAMERA MOVEMENT
+            // PLAYER 2
             // ------------------------------------------------
             KeyCode::ArrowUp => {
                 self.up = pressed;
@@ -75,58 +42,6 @@ impl KeyboardState {
 
             KeyCode::ArrowDown => {
                 self.down = pressed;
-            }
-
-            KeyCode::ArrowLeft => {
-                self.left = pressed;
-            }
-
-            KeyCode::ArrowRight => {
-                self.right = pressed;
-            }
-
-            // ------------------------------------------------
-            // CAMERA ZOOM
-            // ------------------------------------------------
-            KeyCode::Equal => {
-                self.zoom_in = pressed;
-            }
-
-            KeyCode::Minus => {
-                self.zoom_out = pressed;
-            }
-
-            // ------------------------------------------------
-            // SPEED
-            // ------------------------------------------------
-            KeyCode::KeyI => {
-                self.i = pressed;
-            }
-
-            KeyCode::KeyO => {
-                self.o = pressed;
-            }
-
-            // ------------------------------------------------
-            // ROTATION
-            // ------------------------------------------------
-            KeyCode::KeyQ => {
-                self.q = pressed;
-            }
-
-            KeyCode::KeyE => {
-                self.e = pressed;
-            }
-
-            // ------------------------------------------------
-            // SCALE
-            // ------------------------------------------------
-            KeyCode::KeyZ => {
-                self.z = pressed;
-            }
-
-            KeyCode::KeyX => {
-                self.x = pressed;
             }
 
             _ => {}
